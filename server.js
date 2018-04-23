eaca09070b08950335d684128545c70dff4c5e96
montsi <L4784@student.jamk.fi>
2018-04-24 00:16:27 +0300

    script

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const formidable = require('express-formidable');
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 const mysql = require("mysql");
@@ -15,6 +16,7 @@ const connection = mysql.createConnection({
     database: 'L4784_3'
 });
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //Hakee kaikki hoidot
 app.get('/api/hoidot', (req, res) => {
@@ -70,4 +72,6 @@ app.post('/api/varaus',formidable(), function(req, res) {
     
 });
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
